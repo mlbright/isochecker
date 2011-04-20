c5427102dcedb351f69096fb7c89498266cfb3f6
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2011-04-20 11:57:09 -0400

    print out checksum

--- isocheck.py
+++ isocheck.py
@@ -6,18 +6,18 @@ import os.path
 import hashlib
 from subprocess import Popen, PIPE
 
-def process(cmd, block_size=1024*1024):
-    checksum = hashlib.md5()
+def checksum(cmd="dd if=/dev/cdrom", block_size=2**20):
+    cs = hashlib.md5()
     fd = Popen(cmd,shell=True,stdout=PIPE).stdout
     try:
         while True:
             chunk = fd.read(block_size)
             if not chunk:
                 break
-            checksum.update(chunk)
+            cs.update(chunk)
     finally:
         fd.close()    
-    return checksum.hexdigest()
+    return cs.hexdigest()
 
 if __name__ == "__main__":
-    process(cmd="dd if=/dev/cdrom")
+    print checksum()
